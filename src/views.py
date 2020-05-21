017bdbcbf88ee4437cc18594122d4eef284f54d6
strpc <renat.kruchinskij@gmail.com>
2020-05-22 02:19:36 +0300

    get list_company, write to db

--- src/views.py
+++ src/views.py
@@ -2,8 +2,15 @@ from sanic import Blueprint
 from sanic.request import Request
 from sanic.response import json, BaseHTTPResponse as Response
 
-from tools import generate_uuid, uuid_from_str
 import logger
+from tools import (
+    generate_uuid,
+    uuid_from_str,
+    datetime_from_string,
+    uuid_from_str,
+)
+from service import get_list_company
+from db.schema import names_table, parse_company_table, parse_company_table
 
 
 names = Blueprint(
@@ -24,16 +31,46 @@ async def create_task(request: Request) -> Response:
                 "the request with the key 'name'"},
             status=400
         )
+
     local_uuid = generate_uuid()
     try:
-        # TODO: запрос в базу с созданием записи c local_uuid
+        list_company = await get_list_company(name)
+        if list_company is False:
+            raise Exception('list of companies was not received')
+
+        async with request.app.pool.transaction() as conn:
+            names_query = names_table.insert().values(
+                name=name,
+                local_uuid=local_uuid
+            ).returning(names_table)
+            names_value = await conn.fetchrow(names_query)
+
+            for company in list_company:
+                company_query = parse_company_table.insert().values(
+                    link_guid=uuid_from_str(company.get('guid')),
+                    ogrn=int(company.get('ogrn')),
+                    inn=int(company.get('inn')),
+                    name=str(company.get('name')),
+                    address=str(company.get('address')),
+                    status=str(company.get('status')),
+                    status_date=datetime_from_string(company.get('statusDate')),
+                    primary_names_uuid=local_uuid
+                )
+                await conn.fetchrow(company_query)
+
+
+
         # TODO: выполнение парсинга страниц
-        uri_check = request.app.url_for('names.get_information', name=local_uuid)
+
+        uri_check = request.app.url_for(
+            'names.get_information', name=local_uuid
+        )
+        link_for_check = f"{request.scheme}://{request.host}{uri_check}"
         return json(
             {
                 'name': name,
                 'uuid': str(local_uuid),
-                'link for check': f"{request.scheme}://{request.host}{uri_check}",
+                'link for check': link_for_check,
             },
             status=201,
             escape_forward_slashes=False
@@ -86,12 +123,17 @@ async def get_information(request: Request, name: str) -> Response:
 @names.route(
     uri='/',
     methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD']
-    )
+)
 async def index(request: Request) -> Response:
     logger.do_write_info('access to invalid path.', request.ip, request.url)
     return json(
-            {
-                "message": "path does not exist",
-            },
-            status=404,
-        )
+        {
+            "message": "path does not exist",
+        },
+        status=404,
+    )
+
+# FIXME: DEV only
+if __name__ == '__main__':
+    from app import main
+    main()
