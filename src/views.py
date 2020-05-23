a3eb15797f5aed9577fe6997db4bd627d9f84d97
strpc <renat.kruchinskij@gmail.com>
2020-05-23 16:45:12 +0300

    change handler

--- src/views.py
+++ src/views.py
@@ -7,10 +7,9 @@ from tools import (
     generate_uuid,
     uuid_from_str,
     datetime_from_string,
-    uuid_from_str,
 )
-from service import get_list_company
-from db.schema import names_table, parse_company_table, parse_company_table
+from service import get_list_company, get_list_events
+from db.schema import names_table, parse_company_table, parse_event_table
 
 
 names = Blueprint(
@@ -38,7 +37,7 @@ async def create_task(request: Request) -> Response:
         if list_company is False:
             raise Exception('list of companies was not received')
 
-        async with request.app.pool.transaction() as conn:
+        async with request.app.pool.acquire() as conn:  # transaction
             names_query = names_table.insert().values(
                 name=name,
                 local_uuid=local_uuid
@@ -47,20 +46,42 @@ async def create_task(request: Request) -> Response:
 
             for company in list_company:
                 company_query = parse_company_table.insert().values(
-                    link_guid=uuid_from_str(company.get('guid')),
+                    link_guid=company.get('guid', generate_uuid),
                     ogrn=int(company.get('ogrn')),
                     inn=int(company.get('inn')),
                     name=str(company.get('name')),
                     address=str(company.get('address')),
                     status=str(company.get('status')),
-                    status_date=datetime_from_string(company.get('statusDate')),
-                    primary_names_uuid=local_uuid
-                )
-                await conn.fetchrow(company_query)
+                    status_date=datetime_from_string(
+                        company.get('statusDate')
+                    ),
+                    primary_names_id=names_value['id']
+                ).returning(parse_company_table)
+                company_value = await conn.fetchrow(company_query)
 
-
-
-        # TODO: выполнение парсинга страниц
+                list_events = await get_list_events(company.get('guid'))
+                if list_events:
+                    for event in list_events:
+                        # todo: получение текста
+                        event_query = parse_event_table.insert().values(
+                            bankrupt_name=event.get('bankruptName'),
+                            data_publish=datetime_from_string(
+                                event.get('datePublish')
+                            ),
+                            guid=event.get('guid', generate_uuid),
+                            is_annuled=event.get('isAnnuled'),
+                            is_locked=event.get('isLocked'),
+                            is_refuted=event.get('isRefuted'),
+                            number=int(event.get('number')),
+                            publisher_name=event.get('publisherName'),
+                            publisher_type=event.get('publisherType'),
+                            title=event.get('title'),
+                            type=event.get('type'),
+                            text='Lorem',  # event.get(''), ###########
+                            primary_parsed_company_id=company_value['id'],
+                            primary_names_id=names_value['id'],
+                        )
+                        await conn.fetchrow(event_query)
 
         uri_check = request.app.url_for(
             'names.get_information', name=local_uuid
