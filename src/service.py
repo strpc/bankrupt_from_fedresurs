4ace4f8ab95b3e287360437000b162017685e72a
strpc <renat.kruchinskij@gmail.com>
2020-05-24 16:57:51 +0300

    comments, get_text_from_event()

--- src/service.py
+++ src/service.py
@@ -1,9 +1,15 @@
-from tools import request
+from tools import request, get_html, parse_html_for_text
 
 from typing import List
 
 
 async def get_list_company(name: str = None) -> List:
+    """
+    Получения списка компаний по имени.
+
+    :param str name: - имя компании для поиска.
+    :return List: - список словарей компаний с их данными(ИНН, название и тд)
+    """
     if name is None:
         return False
     response = await request(
@@ -17,6 +23,12 @@ async def get_list_company(name: str = None) -> List:
 
 
 async def get_list_events(guid: str = None) -> List:
+    """
+    Получение списка сообщений(с данными) компании по guid
+
+    :param str guid: - уникальный идентификатор компании.
+    :return List: - список словарей сообщений компании с данными.
+    """
     if guid is None:
         return False
     response = await request(
@@ -29,5 +41,19 @@ async def get_list_events(guid: str = None) -> List:
     return False
 
 
-def get_text_from_event():
-    pass
+async def get_text_from_event(guid=None):
+    """
+    Получение(парсинг) блока текста из html-страницы сообщения по guid номеру
+
+    :param str guid: - уникальный идентификатор сообщения.
+    :return str: - возврат текста сообщения, в случае его нахождения, "False"
+    в случае неудачи(или отстутствия текста).
+    """
+    if guid is None:
+        return 'False'
+    text = 'False'
+    try:
+        html = await get_html(guid)
+        text = parse_html_for_text(html)
+    finally:
+        return text
